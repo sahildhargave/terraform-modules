532e9d7ae6a4331456d2303412a39203e3560d11
Sahildhargave <sahildhargave5253.com>
2024-08-01 14:24:19 +0530

    working on the main.tf adding variable to the attributes

--- vpc/main.tf
+++ vpc/main.tf
@@ -23,25 +23,25 @@ data "aws_availability_zones" "available_zones" {}
 
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc_id
+  cidr_block              = public_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "${}-${}-public-az1"
+    Name = "$${var.project_name}-${var.environment}-public-az1"
   }
 }
 
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
+  vpc_id                  = aws_vpc.vpc_id
+  cidr_block              = var.public_subnet_az2_cidr
   availability_zone       = 
   map_public_ip_on_launch = 
 
   tags = {
-    Name = "${}-${}-public-az2"
+    Name = "${var.project_name}-${var.environment}-public-az2"
   }
 }
 
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -16,6 +16,13 @@ variable "environment" {
 
 # vpc variables
 variable "vpc_cidr"{
-	
+
 }
 
+variable "public_subnet_az1_cidr" {
+  
+}
+
+variable "public_subnet_az2_cidr" {
+  
+}
